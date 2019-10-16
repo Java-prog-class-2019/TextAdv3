a502757beab5cc8a1c297ee13c67a46215369100
Isaiah <isaiah.morabito@outlook.com>
2019-10-15 22:10:39 -0400

    Edited main game description.

--- textAdventure/AdventureMain.java
+++ textAdventure/AdventureMain.java
@@ -61,7 +61,7 @@ public class AdventureMain {
 		System.out.println("This is, The Oakville Mystery.");
 		System.out.println("\n\t A Town once known as peaceful and happy until one day people just disappear.\n\t"
 		+ " As the best detective around, you’re asked to take on this case. Follow the clues\n\t"
-		+ " around town to find the (#) missing people and catch the Kidnapper. The town is\n\t"
+		+ " around town to find the five missing people and catch the Kidnapper. The town is\n\t"
 		+ " counting on you to restore the happy and calm reputation.");
 		//starting room
 		currentRoom = "police_station";
