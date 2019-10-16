35af16195dd3a3c775ba84210f7c6ed48b6aa12e
Isaiah <isaiah.morabito@outlook.com>
2019-10-16 10:26:27 -0400

    added to description and added comments in player class

--- textAdventure/Player.java
+++ textAdventure/Player.java
@@ -1,5 +1,8 @@
 package textAdventure;
 
 class Player {
+	
+	//Make food counter for the player and keep the health up 
+	//If the person goes into a main room/store, they lose a certain amount of health 
 
 }
